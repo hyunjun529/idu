eee8bce96d9b47e3cd2c28ae941d0ad053599693
박현준 <hyunjun529@gmail.com>
2016-03-02 11:24:13 +0900

    파일 분리, 들여쓰기 변경

--- js/popup.js
+++ js/popup.js
@@ -10,33 +10,33 @@ function showLinks() {
     var row = document.createElement('div');
     var col0 = document.createElement('div');
     var col1 = document.createElement('div');
-	  
-	row.className += 'row';
-	
-	var checkbox = document.createElement('input');
+
+    row.className += 'row';
+
+    var checkbox = document.createElement('input');
     checkbox.checked = true;
     checkbox.type = 'checkbox';
     checkbox.id = 'check' + i;
-	col0.className += 'col-chk';
+    col0.className += 'col-chk';
     col0.appendChild(checkbox);
-	  
-	col1.className += 'col-txt';
+
+    col1.className += 'col-txt';
     col1.innerText = visibleLinks[i];
-	
-	var col2 = document.createElement('div');
-	var img = document.createElement('img');
-	img.src = visibleLinks[i];
-	img.addEventListener("click", function(){
-		console.debug(this.src);
-		chrome.downloads.download({url: this.src}, function(id) { });
-	});
-	col2.className += 'col-img';
-	col2.appendChild(img);
-	
-    row.appendChild(col0);
-    row.appendChild(col1);
-	row.appendChild(col2);
-	  
+
+    var col2 = document.createElement('div');
+    var img = document.createElement('img');
+    img.src = visibleLinks[i];
+    img.addEventListener("click", function(){
+      console.debug(this.src);
+      chrome.downloads.download({url: this.src}, function(id) { });
+    });
+    col2.className += 'col-img';
+    col2.appendChild(img);
+
+    //row.appendChild(col0);
+    //row.appendChild(col1);
+    row.appendChild(col2);
+
     linksTable.appendChild(row);
   }
 }
@@ -51,11 +51,11 @@ chrome.extension.onRequest.addListener(function(links) {
 });
 
 window.onload = function() {
-	chrome.windows.getCurrent(function (currentWindow) {
+  chrome.windows.getCurrent(function (currentWindow) {
     chrome.tabs.query({active: true, windowId: currentWindow.id},
-                      function(activeTabs) {
-      chrome.tabs.executeScript(
-        activeTabs[0].id, {file: 'extract_imageURLs.js', allFrames: true});
-    });
+      function(activeTabs) {
+        chrome.tabs.executeScript(
+          activeTabs[0].id, {file: 'js/extract_imageURLs.js', allFrames: true});
+      });
   });
 };
