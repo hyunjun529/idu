90a17f1915d55cab190f9eccb154364352394c26
Hyunjun529 <hyunjun529@gmail.com>
2016-03-17 11:45:24 +0900

    shoud 함수 사용방법 변경, 실패하는 테스트 작성

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var expect = chai.expect;
 var should = chai.should();
 
 describe('Extract Img Tag', function(){
-  
+
   const extractor = {
     res : imgTagExtraction(),
     check : (str => res.find(e => e.search(str) >= 0))
@@ -11,22 +11,22 @@ describe('Extract Img Tag', function(){
   describe('check list of extracting tags', function(){
 
     it('extract .png to Img Tag', function(){
-      should.exist(extractor.check('.png'));
+      extractor.check('.png').should.exist;
     });
     
     it('extract .jpg to Img Tag', function(){
-      should.exist(extractor.check('.jpg'));
+      extractor.check('.jpg').should.exist;
     });
     
     it('extract .gif to Img Tag', function(){
-      should.exist(extractor.check('.gif'));
+      extractor.check('.gif').should.exist;
     });
     
     it('extract .jpeg to Img Tag', function(){
-      should.exist(extractor.check('.jpeg'));
+      extractor.check('.jpeg').should.exist;
     });
   });
-    
+
   describe('exception handling extracted url from src', function(){
 
     it('filter location hash to URL', function(){
@@ -34,7 +34,7 @@ describe('Extract Img Tag', function(){
     });
 
     it('parsing relative path', function(){
-      should.exist(extractor.check('.jpg'));
+      extractor.check('.jpg').should.exist;
     });
     
     xit('twitter:large img; PENDING: twitter img thumb path is so exceptional', function(){
@@ -53,41 +53,62 @@ describe('Extract Video Tag', function(){
   describe('check list of extracting tags', function(){
 
     it('extract .mp4 to Video Tag has Source Tag', function(){
-      should.exist(extractor.check('.mp4'));
+      extractor.check('.mp4').should.exist;
     });
 
     it('extract .webm to Video Tag has src attr', function(){
-      should.exist(extractor.check('.webm'));
+      extractor.check('.webm').should.exist;
     });
   });
 });
 
 describe('Extract CSS Prop', function(){
-  it('extract .png to CSS', function(){
 
+  const extractor = {
+    res : cssBackgroundExtraction(),
+    check : (str => res.find(e => e.search(str) >= 0))
+  };
+
+  describe('check list of extracting tags', function(){
+    
+    it('extract .png to CSS', function(){
+      extractor.check('.png').should.exist;
+    });
+
+    it('extract .jpg to CSS', function(){
+      extractor.check('.jpg').should.exist;
+    });
+    
+    it('extract .gif to CSS', function(){
+      extractor.check('.gif').should.exist;
+    });
+    
+    it('extract .jpeg to CSS', function(){
+      extractor.check('.jpeg').should.exist;
+    });    
   });
-  it('extract .jpg to CSS', function(){
 
-  });
-  it('extract .gif to CSS', function(){
+  describe('is removed property that not formated url?', function(){
+    
+    it('filter inherit to CSS', function(){
+      should.not.exist(extractor.check('inherit'));
+    });
+    
+    it('filter initial to CSS', function(){
+      should.not.exist(extractor.check('initial'));
+    });
+    
+    it('complete URL to relative path', function(){
+      should.not.exist(extractor.check('..'));
+    });
+    
+    it('complete URL to withour root url ', function(){
+      var hasNotHTTP = extractor.res.map(e => e.search("http") !== -1);
+      hasNotHTTP.should.not.contain(false);
+    });
 
-  });
-  it('extract .jpeg to CSS', function(){
+    it('filter color', function(){
 
-  });
-  it('filter inherit to CSS', function(){
-
-  });
-  it('filter initial to CSS', function(){
-
-  });
-  it('complete URL to relative path', function(){
-
-  });
-  it('complete URL to withour root url ', function(){
-
-  });
-  it('filter color', function(){
-
+    });
   });
 });
