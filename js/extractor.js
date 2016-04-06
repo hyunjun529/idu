20b38b87abfbad0a54c5576db55d89fc97608ca0
Hyunjun529 <hyunjun529@gmail.com>
2016-04-06 22:03:47 +0900

    리펙토링 : 변수 스코프 변경, 현재 새로고침하면 동작하지 않는 버그 발견, 내가 let을 잘못 쓴건가?; 예전엔 괜찮았는데 원인 찾아봐야겠음

--- js/extractor.js
+++ js/extractor.js
@@ -1,53 +1,42 @@
 var extractedURL = [];
-var regForDataImage = new RegExp(/data:image/i);
+const regForDataImage = new RegExp(/data:image/i);
 
 function imgTagExtraction(){
-  var tags;
-
+  let tags;
   tags = Array.from(document.querySelectorAll('img'));
   tags = tags.map(e => e.src.split("#")[0]);
-  
   return tags;
 }
 
 function videoTagExtraction(){
-  var tags;
-  
+  let tags;
   tags = Array.from(document.querySelectorAll('video'));
   tags = tags.map(e => {
-    var videoSrc;
-    
-    videoSrc = e.src;
-    
+    let videoSrc = e.src;
     if(!videoSrc || videoSrc === ""){
       if(e.getElementsByTagName("source").length <= 0){
         videoSrc = "";
       } else {
-        if(e.getElementsByTagName("source")[0].hasAttribute('src'))
+        if(e.getElementsByTagName("source")[0].hasAttribute('src')){
           videoSrc = e.getElementsByTagName("source")[0].src;
+        }
       }
     }
-    
     videoSrc = videoSrc.split("#")[0];
-    
     return videoSrc;
   });
-
   return tags;
 }
 
 function cssBackgroundExtraction(){
-  var links = [];
-  var aryPathname = window.location.pathname.split('/').filter(v => v !== "");
-  var i, j;
-
+  let aryPathname = window.location.pathname.split('/').filter(v => v !== ""),
+      links = [],
+      i, j;
   for (i = 0; i < document.styleSheets.length; i++) {
-    var cssRules = document.styleSheets[i].cssRules;
-
+    let cssRules = document.styleSheets[i].cssRules;
     if (cssRules) {
       for (j = 0; j < cssRules.length; j++) {
-        var style = cssRules[j].style;
-
+        let style = cssRules[j].style;
         if (style) {
           if(style.backgroundImage){
             if(style.backgroundImage.search('url') < 0){
@@ -59,19 +48,14 @@ function cssBackgroundExtraction(){
       }
     }
   }
-
   links = links.filter(v => v !== "none");
   links = links.filter(v => v !== "initial");
   links = links.filter(v => v !== "inherit");
   links = links.map(e =>{
-    
-    var cntCallingParent;
-    var fullUrl;
-
+    let cntCallingParent;
+    let fullUrl;
     cntCallingParent = e.split('..').length;
-    
     fullUrl = window.location.protocol + "//" + window.location.host;
-    
     if(e.search('http') > -1) {
       fullUrl = e;
     } else if(e.search('//') > -1){
@@ -79,29 +63,28 @@ function cssBackgroundExtraction(){
     } else if(e[0] === '/'){
       fullUrl += e;
     } else {
-      for(var i = 0; i < cntCallingParent - 1; i++){
+      for(i = 0; i < cntCallingParent - 1; i++){
         fullUrl += '/' + aryPathname[i];
       }
-      for(var i = 0; i < cntCallingParent - 1; i++){
+      for(i = 0; i < cntCallingParent - 1; i++){
         e = e.substring(3);
       }
       fullUrl += '/' + e;
     }
-
     return fullUrl;
   });
-
   return links;
 }
 
 extractedURL =
-imgTagExtraction()
-.concat(cssBackgroundExtraction())
-.concat(videoTagExtraction());
+  imgTagExtraction()
+  .concat(videoTagExtraction());
+  //.concat(cssBackgroundExtraction());
 
 extractedURL = extractedURL.filter(v => v != "");
 extractedURL = extractedURL.filter(v => !regForDataImage.test(v));
-
 extractedURL = extractedURL.filter((i, v) => extractedURL.indexOf(i) == v);
 
 chrome.extension.sendRequest(extractedURL);
+
+extractedURL = null;
